6369ed9fdcee0a2599667b36a0c060711e2388f0
Roberto Rodriguez <roberto.rodriguez@alumni.colostate.edu>
2021-02-16 15:08:49 -0700

    Refactor world creation and cell generation

--- lib/grid.rb
+++ lib/grid.rb
@@ -1,17 +1,16 @@
 require '../lib/cell'
+require 'pry'
 
 class Grid
     attr_reader :world, :col1, :col2, :col3
     def initialize
-        @world = [@col1= [], @col2= [], @col3= []]
+        @world = Array.new(3)
     end
 
     def generate_cells
-        @world.each do |column|
-            3.times do
-                column << Cell.new
-            end
-        end
+        @world[0] = Array.new(3, Cell.new)
+        @world[1] = Array.new(3, Cell.new)
+        @world[2] = Array.new(3, Cell.new)
     end
 
     def count_active_neighbors
@@ -27,4 +26,14 @@ class Grid
         end
         p "There are #{counter} active neighbors" 
     end
+
+    def start
+        generate_cells
+        puts 'Welcome to Count My Neighbors'
+        puts 'Rules: You have a 3x3 grid, select a cell to count its active neighbors!'
+        puts 'Cells: A = top left, B = top middle, C = top right, D = middle left, E = center, F = middle right, G = bottom left, H = bottom middle, I = bottom right'
+        puts 'Enter the letter of the cell you wish to begin with'
+        print "> "
+        answer = gets.chomp.upcase
+    end 
 end
--- test/grid_test.rb
+++ test/grid_test.rb
@@ -29,24 +29,24 @@ class GridTest < MiniTest::Test
         assert_instance_of Grid, @grid
     end
 
-    def test_grid_begins_with_no_cells
-        assert_equal 0, @grid.col1.length
-        assert_equal 0, @grid.col2.length
-        assert_equal 0, @grid.col3.length
+    def test_grid_begins_with_attributes
+        # this is testing that the grid begins with an array with 3 rows
+        expected = [nil, nil, nil]
+        assert_equal expected, @grid.world
     end
 
     def test_cells_can_be_generated
         @grid.generate_cells
-        assert_equal 3, @grid.col1.length
-        assert_equal 3, @grid.col2.length
-        assert_equal 3, @grid.col3.length
+        assert_equal 3, @grid.world[0].length
+        assert_equal 3, @grid.world[1].length
+        assert_equal 3, @grid.world[2].length
     end
 
     # I am using mocks / stubs here for the cells active status to determine if count method works
     def test_grid_can_count_active_cells
-        @grid.col1.push(@cell, @cell2, @cell3)
-        @grid.col2.push(@cell4, @cell5, @cell6)
-        @grid.col3.push(@cell7, @cell8, @cell9)
+        @grid.world[0] = Array.new.push(@cell, @cell2, @cell3)
+        @grid.world[1] = Array.new.push(@cell4, @cell5, @cell6)
+        @grid.world[2] = Array.new.push(@cell7, @cell8, @cell9)
         expected = "There are 5 active neighbors"
         assert_equal expected, @grid.count_active_neighbors
     end
