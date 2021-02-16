ecdd5e75b29246ae69586e1316c6239cbe394ab4
Roberto Rodriguez <roberto.rodriguez@alumni.colostate.edu>
2021-02-16 10:09:24 -0700

    Remove test to check that grid begins with three empty columns. Test no longer relevant due to adding generate cells method to initialize

--- lib/grid.rb
+++ lib/grid.rb
@@ -6,5 +6,14 @@ class Grid
         @column1 = []
         @column2 = []
         @column3 = []
+        generate_cells
+    end
+
+    def generate_cells
+        3.times do
+            @column1 << Cell.new
+            @column2 << Cell.new
+            @column3 << Cell.new
+        end
     end
 end
--- test/grid_test.rb
+++ test/grid_test.rb
@@ -10,12 +10,6 @@ class GridTest < MiniTest::Test
         assert_instance_of Grid, @grid
     end
 
-    def test_a_grid_begins_with_three_empty_columns
-        assert_equal [], @grid.column1
-        assert_equal [], @grid.column2
-        assert_equal [], @grid.column3
-    end
-
     def test_cells_are_generated_once_grid_is_initialized
         assert_equal 3, @cell.column1.length
         assert_equal 3, @cell.column2.length
