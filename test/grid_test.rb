86bb472514a40ac96fc2240434e0a52f71e6084b
Roberto Rodriguez <roberto.rodriguez@alumni.colostate.edu>
2021-02-16 10:10:32 -0700

    Fix typo in test. Changed @cell to @grid

--- test/grid_test.rb
+++ test/grid_test.rb
@@ -11,8 +11,8 @@ class GridTest < MiniTest::Test
     end
 
     def test_cells_are_generated_once_grid_is_initialized
-        assert_equal 3, @cell.column1.length
-        assert_equal 3, @cell.column2.length
-        assert_equal 3, @cell.column3.length
+        assert_equal 3, @grid.column1.length
+        assert_equal 3, @grid.column2.length
+        assert_equal 3, @grid.column3.length
     end
 end
