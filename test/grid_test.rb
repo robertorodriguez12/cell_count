cd9858b7752c3ca6bbf4a146a113e5467ac36637
Roberto Rodriguez <roberto.rodriguez@alumni.colostate.edu>
2021-02-16 10:03:20 -0700

    Add test for auto generation of cells once grid is initialized

--- test/grid_test.rb
+++ test/grid_test.rb
@@ -15,4 +15,10 @@ class GridTest < MiniTest::Test
         assert_equal [], @grid.column2
         assert_equal [], @grid.column3
     end
+
+    def test_cells_are_generated_once_grid_is_initialized
+        assert_equal 3, @cell.column1.length
+        assert_equal 3, @cell.column2.length
+        assert_equal 3, @cell.column3.length
+    end
 end
