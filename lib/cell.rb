9bf554e9c0063a798d53ff7a737266c58873dc34
Roberto Rodriguez <roberto.rodriguez@alumni.colostate.edu>
2021-02-16 16:25:38 -0700

    Add coordinate attribute to cell to facilitate search function in grid

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,10 +1,15 @@
 class Cell
     attr_reader :active 
-    def initialize
+    def initialize(coordinate)
+        @coordinate = coordinate
         @active = [true, false].sample
     end
 
     def active?
         @active
     end
+
+    def render 
+        return @coordinate
+    end 
 end
--- test/cell_test.rb
+++ test/cell_test.rb
@@ -2,14 +2,19 @@ require './test_helper'
 require '../lib/cell'
 
 class CellTest < MiniTest::Test
+    def setup
+        @cell = Cell.new('A1')
+    end
     def test_it_exists
-        cell = Cell.new
-        assert_instance_of Cell, cell
+        assert_instance_of Cell, @cell
     end
     
     def test_it_begins_with_random_active_status
-        cell = Cell.new
         expected = [true, false] 
-        assert_includes expected, cell.active?
+        assert_includes expected, @cell.active?
+    end
+
+    def test_cell_coordinate_can_be_rendered
+        assert_equal 'A1', @cell.render
     end
 end 
