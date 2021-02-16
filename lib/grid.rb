8cc62293ae226e7051c52f8e644cb1b78b43bcf6
Roberto Rodriguez <roberto.rodriguez@alumni.colostate.edu>
2021-02-16 16:50:38 -0700

    Adjust grid class structure. Move search methods to new class

--- lib/grid.rb
+++ lib/grid.rb
@@ -2,38 +2,25 @@ require '../lib/cell'
 require 'pry'
 
 class Grid
-    attr_reader :world, :col1, :col2, :col3
+    attr_reader :cells
     def initialize
-        @world = Array.new(3)
+        @cells = {}
+        generate_cells
     end
 
     def generate_cells
-        @world[0] = Array.new(3, Cell.new)
-        @world[1] = Array.new(3, Cell.new)
-        @world[2] = Array.new(3, Cell.new)
+        letters = ['A', 'B', 'C']
+        numbers = ['1', '2', '3']
+        letters.each do |letter|
+            numbers.each do |number|
+                @cells[letter+number] = Cell.new(letter + number)
+            end 
+        end 
     end
 
-    def count_active_neighbors
-        center = @col2[1]
-        counter = 0
-        @world.each do |column|
-            column.each do |cell|
-                next if cell == center
-                if cell.active == true
-                    counter += 1
-                end 
-            end
-        end
-        p "There are #{counter} active neighbors" 
+    def valid_selection(key)
+        @cells.has_key?(key)
     end
+   
 
-    def start
-        generate_cells
-        puts 'Welcome to Count My Neighbors'
-        puts 'Rules: You have a 3x3 grid, select a cell to count its active neighbors!'
-        puts 'Cells: A = top left, B = top middle, C = top right, D = middle left, E = center, F = middle right, G = bottom left, H = bottom middle, I = bottom right'
-        puts 'Enter the letter of the cell you wish to begin with'
-        print "> "
-        answer = gets.chomp.upcase
-    end 
 end
--- test/grid_test.rb
+++ test/grid_test.rb
@@ -1,53 +1,24 @@
 require './test_helper'
-require 'mocha/minitest'
 require '../lib/grid'
 
 class GridTest < MiniTest::Test 
     def setup
         @grid = Grid.new
-        @cell = mock
-        @cell.stubs(:active).returns(true)
-        @cell2 = mock
-        @cell2.stubs(:active).returns(true)
-        @cell3 = mock
-        @cell3.stubs(:active).returns(true)
-        @cell4 = mock
-        @cell4.stubs(:active).returns(false)
-        @cell5 = mock
-        @cell5.stubs(:active).returns(false)
-        @cell6 = mock
-        @cell6.stubs(:active).returns(true)
-        @cell7 = mock
-        @cell7.stubs(:active).returns(true)
-        @cell8 = mock
-        @cell8.stubs(:active).returns(false)
-        @cell9 = mock
-        @cell9.stubs(:active).returns(false)
     end 
 
     def test_it_exists
         assert_instance_of Grid, @grid
     end
-
-    def test_grid_begins_with_attributes
-        # this is testing that the grid begins with an array with 3 rows
-        expected = [nil, nil, nil]
-        assert_equal expected, @grid.world
-    end
-
+    
     def test_cells_can_be_generated
-        @grid.generate_cells
-        assert_equal 3, @grid.world[0].length
-        assert_equal 3, @grid.world[1].length
-        assert_equal 3, @grid.world[2].length
+        expected = ['A1', 'A2', 'A3',
+                 'B1', 'B2', 'B3',
+                'C1', 'C2', 'C3']
+        assert_equal expected, @grid.cells.keys
     end
 
-    # I am using mocks / stubs here for the cells active status to determine if count method works
-    def test_grid_can_count_active_cells
-        @grid.world[0] = Array.new.push(@cell, @cell2, @cell3)
-        @grid.world[1] = Array.new.push(@cell4, @cell5, @cell6)
-        @grid.world[2] = Array.new.push(@cell7, @cell8, @cell9)
-        expected = "There are 5 active neighbors"
-        assert_equal expected, @grid.count_active_neighbors
+    def test_grid_has_valid_cell_coordinates
+        assert_equal true, @grid.valid_selection('A1')
+        assert_equal false, @grid.valid_selection('D1')
     end
 end
