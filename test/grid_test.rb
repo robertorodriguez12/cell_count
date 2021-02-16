89e3f4ad0428e5f1f229757c39d963d77cf3c96e
Roberto Rodriguez <roberto.rodriguez@alumni.colostate.edu>
2021-02-16 10:20:56 -0700

    Create test to ensure center cell is set

--- test/grid_test.rb
+++ test/grid_test.rb
@@ -15,4 +15,10 @@ class GridTest < MiniTest::Test
         assert_equal 3, @grid.column2.length
         assert_equal 3, @grid.column3.length
     end
+
+    def test_center_cell_is_set
+        expected = @grid.column2[1]
+        binding.pry
+        assert_equal expected, @grid.center_cell
+    end
 end
