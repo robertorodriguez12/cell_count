9f1dfaea1c5aecd212f00179c310a3f7f1f11d26
Roberto Rodriguez <roberto.rodriguez@alumni.colostate.edu>
2021-02-17 10:05:34 -0700

    Add test for search method

--- test/grid_test.rb
+++ test/grid_test.rb
@@ -15,4 +15,33 @@ class GridTest < MiniTest::Test
         assert_equal 3, @grid.cells[1].length
         assert_equal 3, @grid.cells[2].length
     end
+
+    def test_game_can_count_active_neighbors
+        grid = mock
+        grid.stubs(:cells).returns([[], [], []])
+        cell = mock
+        cell.stubs(:active).returns(true)
+        cell2 = mock
+        cell2.stubs(:active).returns(true)
+        cell3 = mock
+        cell3.stubs(:active).returns(true)
+        cell4 = mock
+        cell4.stubs(:active).returns(false)
+        cell5 = mock
+        cell5.stubs(:active).returns(false)
+        cell6 = mock
+        cell6.stubs(:active).returns(true)
+        cell7 = mock
+        cell7.stubs(:active).returns(true)
+        cell8 = mock
+        cell8.stubs(:active).returns(false)
+        cell9 = mock
+        cell9.stubs(:active).returns(false)
+        grid[0].push(cell, cell2, cell3)
+        grid[1].push(cell4, cell5, cell6)
+        grid[2].push(cell7, cell8, cell9)
+        expected = "There are 5 active neighbors"
+        assert_equal expected, grid.search_neighbors
+    end
+
 end
