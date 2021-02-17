442cc417c87f5defc868820baadadeb45453a9f8
Roberto Rodriguez <roberto.rodriguez@alumni.colostate.edu>
2021-02-17 14:53:43 -0700

    Grid search neighbors method complete and tested

--- lib/grid.rb
+++ lib/grid.rb
@@ -1,11 +1,11 @@
 require '../lib/cell'
 require 'pry'
+require 'matrix'
 
 class Grid
     attr_reader :cells
     def initialize
-        @cells = Array.new(3)
-        generate_cells
+        @cells = [[], [], []]
     end
 
     def generate_cells
@@ -18,5 +18,62 @@ class Grid
             @cells[1] << Cell.new('B' + number)
             @cells[2] << Cell.new('C' + number)
         end 
+    end
+
+    def search_neighbors(cell)
+        counter = 0
+        index = Matrix[*cells].index(cell)
+        starting_row = @cells[index[0]]
+        min_x = false
+        max_x = false
+        min_y = false
+        max_y = false
+        # left / right search
+        if index[0] != 0
+            counter += 1 if starting_row[index[0]- 1].active == true
+        else 
+            min_x = true
+        end
+        
+        # binding.pry
+        if starting_row[index[0] + 1].nil? == false
+            counter += 1 if starting_row[index[0]+ 1].active == true 
+        else
+            max_x = true 
+        end
+        
+        # up / down search
+        if index[1] != 0
+            counter += 1 if @cells[index[0]][index[1] - 1].active == true
+        else
+            min_y = true  
+        end
+        
+        if @cells[index[1]][index[1] + 1].nil? == false
+            counter += 1 if @cells[index[0]][index[1] + 1].active == true
+        else
+            max_y = true  
+        end
+        
+        # up right search @cells[index[0] + 1][index[1] - 1].nil? == false ||
+        if  max_x != true && min_y != true
+            counter += 1 if @cells[index[0] + 1][index[1] - 1].active == true  
+        end
+               
+        # up left
+        if min_x != true && min_y != true
+            counter += 1 if @cells[index[0] - 1][index[1] - 1].active == true  
+        end
+
+        # down left search
+        if min_x != true && max_y != true
+            counter += 1 if @cells[index[0] - 1][index[1] + 1].active == true  
+        end
+
+        # down right
+        if max_x != true && max_y != true
+            counter += 1 if @cells[index[0] + 1][index[1] + 1].active == true  
+        end
+        p "There are #{counter} active neighbors"
     end
 end
--- test/grid_test.rb
+++ test/grid_test.rb
@@ -11,37 +11,48 @@ class GridTest < MiniTest::Test
     end
     
     def test_cells_can_be_generated
+        @grid.generate_cells
         assert_equal 3, @grid.cells[0].length
         assert_equal 3, @grid.cells[1].length
         assert_equal 3, @grid.cells[2].length
     end
 
     def test_game_can_count_active_neighbors
-        grid = mock
-        grid.stubs(:cells).returns([[], [], []])
+        @grid.cells[0] = []
+        @grid.cells[1] = []
+        @grid.cells[2] = []
         cell = mock
         cell.stubs(:active).returns(true)
+        cell.stubs(:coordinate).returns('A1')
         cell2 = mock
         cell2.stubs(:active).returns(true)
+        cell2.stubs(:coordinate).returns('A2')
         cell3 = mock
         cell3.stubs(:active).returns(true)
+        cell3.stubs(:coordinate).returns('A3')
         cell4 = mock
         cell4.stubs(:active).returns(false)
+        cell4.stubs(:coordinate).returns('B1')
         cell5 = mock
         cell5.stubs(:active).returns(false)
+        cell5.stubs(:coordinate).returns('B2')
         cell6 = mock
         cell6.stubs(:active).returns(true)
+        cell6.stubs(:coordinate).returns('B3')
         cell7 = mock
         cell7.stubs(:active).returns(true)
+        cell7.stubs(:coordinate).returns('C1')
         cell8 = mock
         cell8.stubs(:active).returns(false)
+        cell8.stubs(:coordinate).returns('C2')
         cell9 = mock
         cell9.stubs(:active).returns(false)
-        grid[0].push(cell, cell2, cell3)
-        grid[1].push(cell4, cell5, cell6)
-        grid[2].push(cell7, cell8, cell9)
+        cell9.stubs(:coordinate).returns('C3')
+        @grid.cells[0].push(cell, cell2, cell3)
+        @grid.cells[1].push(cell4, cell5, cell6)
+        @grid.cells[2].push(cell7, cell8, cell9)
         expected = "There are 5 active neighbors"
-        assert_equal expected, grid.search_neighbors
+        assert_equal expected, @grid.search_neighbors(cell5)
     end
 
 end
